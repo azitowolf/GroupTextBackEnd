c06eb51a41151d0428db6370210855e7c39e7f05
azitowolf <azito.wolf3@gmail.com>
2015-06-29 09:55:44 -0400

    heroku deploy recreated seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,11 +15,11 @@ andrew = User.create! name:"Andrew", image:"http://www.c4dcafe.com/ipb/uploads/p
 p1 = alex.ptexts.create! history:"#1: Hi! #2: hey there world!", text:"im moving to kansas", number: 6172
 p2 = max.ptexts.create! history:"#1: I was spending all day coding and napping with my computer #2: I am pissed!", text:"im moving to kansas", number: 6172
 
-# s1 = p1.stexts.create! user: alex, text:"Alex Z needs a place to stay, hes clean and well mannered"
-# s2 = p2.stexts.create! user: max, text:"sorry babe, but honestly you just dont look as good in a skirt as this web app looks with a fresh authentication gem"
-# # s3 = p3.stexts.create! user:andrew, title:'good call', text:"Hi barkeep, I may have lost my card at your place, could I flop on over and pick it up?"
+s1 = p1.stexts.create! user: alex, text:"Alex Z needs a place to stay, hes clean and well mannered"
+s2 = p2.stexts.create! user: max, text:"sorry babe, but honestly you just dont look as good in a skirt as this web app looks with a fresh authentication gem"
+# s3 = p3.stexts.create! user:andrew, title:'good call', text:"Hi barkeep, I may have lost my card at your place, could I flop on over and pick it up?"
 
-# s1.liked_by alex
-# s2.liked_by alex
+s1.liked_by alex
+s2.liked_by alex
 
 
